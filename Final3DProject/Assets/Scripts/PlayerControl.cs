3ad043ec08c1232f93ea128ed92b0817b9688561
Brent Van Daele <brent.vandaele@live.be>
2023-12-07 15:44:48 +0100

    player updated

--- Final3DProject/Assets/Scripts/PlayerControl.cs
+++ Final3DProject/Assets/Scripts/PlayerControl.cs
@@ -8,10 +8,18 @@ namespace Assets.Scripts
     public class PlayerControl : MonoBehaviour
     {
         public GameObject player;
-        public float speed = 1;
+        public float walkingSpeed = 1;
+        public float rotateSpeed = 100;
+
+        Animator animator;
 
         float horizontal = 0;
         float vertical = 0;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
 
         void Update()
         {
@@ -23,8 +31,11 @@ namespace Assets.Scripts
         {
             if (player != null)
             {
-                transform.Translate(speed * Time.deltaTime * vertical * Vector3.forward);
-                transform.Translate(speed * Time.deltaTime * horizontal * Vector3.left);
+                if (horizontal != 0 || vertical != 0) {
+                    animator.SetTrigger("Walk");
+                }
+                transform.Translate(walkingSpeed * Time.deltaTime * vertical * Vector3.forward);
+                transform.Rotate(rotateSpeed * Time.deltaTime * horizontal * Vector3.up);
             }
         }
     }
---
+++ Final3DProject/Assets/Scripts/TrashBin.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class TrashBin : MonoBehaviour
+    {
+        Animator animator;
+
+        void Start()
+        {
+            animator = GetComponent<Animator>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (animator != null && collision.gameObject.tag == "Player")
+            {
+                animator.SetBool("Open", true);
+            }
+        }
+
+        private void OnCollisionExit(Collision collision)
+        {
+            if (animator != null && collision.gameObject.tag == "Player")
+            {
+                animator.SetBool("Open", false);
+            }
+        }
+    }
+}
