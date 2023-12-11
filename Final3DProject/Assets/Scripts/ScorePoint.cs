8d03f41109f1571eaabe376407baf668e174d006
oyasso <devochtyasson1@gmail.com>
2023-12-12 00:12:59 +0100

    Score sound, Nets Added

--- Final3DProject/Assets/Scripts/ScorePoint.cs
+++ Final3DProject/Assets/Scripts/ScorePoint.cs
@@ -2,36 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveTrash : MonoBehaviour
+public class ScorePoint : MonoBehaviour
 {
     public AudioClip audioClip;
-    private float spaceBetweenBins = 1f;
-    private int binAmount = 4;
-    private int poszFirstBin = 49;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // calculates the position of the farthest bin, depending on the number of bins and the distance betweens bins
-        if (Input.GetKeyDown(KeyCode.RightArrow) && transform.position.z > poszFirstBin - binAmount*spaceBetweenBins + 1*spaceBetweenBins)
-        {
-            transform.Translate(0, 0, -spaceBetweenBins); // move ball one bin right
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.z < 49)
-        {
-            transform.Translate(0, 0, spaceBetweenBins); // move ball one bin left
-        }
-        // drop the ball in the bin
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            GetComponent<Rigidbody>().useGravity = true;
-        }
+        
     }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == tag)
@@ -49,7 +34,7 @@ public class MoveTrash : MonoBehaviour
                     AudioSource.PlayClipAtPoint(audioClip, transform.position);
                 }
             }
-            Destroy(gameObject);
         }
+        Destroy(other.gameObject);
     }
 }
